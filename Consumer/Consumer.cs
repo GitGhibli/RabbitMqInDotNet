e5443977142b0eecc8ea87d3b6ce14aa4b8ac179
Martin Galajda <martingalajdajob@gmail.com>
2020-10-14 14:18:18 +0200

    Publish consume broadcast queues

---
+++ Consumer/Consumer.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Consumer
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+                //In the .NET client, when we supply no parameters to QueueDeclare() we create a non-durable, exclusive, autodelete queue with a generated name:
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queueName, "logs", routingKey: "");
+                
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+
+                channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+                Console.WriteLine("Waiting for logs");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ Publisher/Publisher.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Publisher
+{
+    class Publisher
+    {
+        static void Main()
+        {
+            var connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+                var message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "logs", routingKey: "", basicProperties: null, body: body);
+
+                Console.WriteLine("message sent");
+            }
+        }
+    }
+}
